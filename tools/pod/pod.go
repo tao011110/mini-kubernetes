05a9e08e448878568029e46054b2747b99a37a01
jingkai he <he-jingkai@outlook.com>
2022-04-12 14:53:45 +0800

    small change(nodename->nodeid)

--- tools/pod/pod.go
+++ tools/pod/pod.go
@@ -3,70 +3,70 @@
 package pod
 
 type Volume struct {
-	Name string
+	Name     string
 	HostPath string
 }
 
 type VolumeMount struct {
-	Name string
+	Name      string
 	MountPath string
 }
 
 type PortMapping struct {
-	Name string
+	Name          string
 	ContainerPort uint16
-	HostPort uint16
-	Protocol string
+	HostPort      uint16
+	Protocol      string
 }
 
 type Resource struct {
-	CPU uint16
+	CPU    uint16
 	Memory uint64
 }
 
 type Container struct {
-	Name string
-	Image string
-	Commands []string
-	Args []string
-	WorkingDir string
-	VolumeMounts []VolumeMount
-	PortMappings []PortMapping
-	ResourceLimit Resource
+	Name            string
+	Image           string
+	Commands        []string
+	Args            []string
+	WorkingDir      string
+	VolumeMounts    []VolumeMount
+	PortMappings    []PortMapping
+	ResourceLimit   Resource
 	ResourceRequest Resource
 }
 
 type HttpHeaderPair struct {
-	Name string
+	Name  string
 	Value string
 }
 
 type HttpRequest struct {
-	Scheme string
+	Scheme      string
 	HttpHeaders []HttpHeaderPair
-	Path string
-	Port uint16
+	Path        string
+	Port        uint16
 }
 
 type Command string
 
 type LivenessProbe struct {
-	InitialDelaySeconds  uint32
-	TimeoutSeconds       uint32
-	PeriodSeconds        uint32
-	FailureThreshold     uint32
-	SuccessThreshold     uint32
+	InitialDelaySeconds uint32
+	TimeoutSeconds      uint32
+	PeriodSeconds       uint32
+	FailureThreshold    uint32
+	SuccessThreshold    uint32
 
-	HttpHeaders []HttpHeaderPair
-	Command Command
+	HttpHeaders    []HttpHeaderPair
+	Command        Command
 	HttpGetRequest HttpRequest
 }
 
 type Pod struct {
-	Name string
-	Containers []Container
-	Volumes []Volume
+	Name          string
+	Containers    []Container
+	Volumes       []Volume
 	LivenessProbe LivenessProbe
-	NodeName string
+	NodeID        uint64
 	//nodeSelector 迭代三再加
 }
