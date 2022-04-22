1c41f459ed8d6df4b37eef48ff7c97c7a8bc1564
陶昱丞 <1501927615@qq.com>
2022-04-22 23:46:03 +0800

    add IP for the pod and its container

--- test/docker_test/docker_test.go
+++ test/docker_test/docker_test.go
@@ -1,21 +1,21 @@
 package docker_test
 
 import (
-	"fmt"
 	"github.com/YOUR-USER-OR-ORG-NAME/YOUR-REPO-NAME/tools/docker"
 	"testing"
 )
 
 func Test(t *testing.T) {
-	path := "./container/docker_test3.yaml"
+	path := "./docker_test3.yaml"
 
-	containerIDs := docker.CreateContrainer(path)
+	containerIDs := docker.CreateContrainer(path, "172.18.0.0")
 
 	for _, id := range containerIDs {
-		fmt.Printf("has created: %s\n", id)
+		t.Logf("has created: %s\n", id)
 		docker.StartContainer(id)
 		//container.RestartContainer(id)
 		//container.StopContainer(id)
 		//container.RemoveContainer(id)
 	}
+	t.Log("test finished\n")
 }
