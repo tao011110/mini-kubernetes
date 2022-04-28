c66be409ff19e1cf443e9643244324b95f044d85
linshuhuai <linshuhuai@sjtu.edu.cn>
2022-04-28 11:54:02 +0800

    add check etcd has started or not

--- tools/etcd/etcdstart.go
+++ tools/etcd/etcdstart.go
@@ -1,21 +1,43 @@
 package etcd
 
 import (
+	"bytes"
 	"context"
 	"fmt"
-	clientv3 "go.etcd.io/etcd/client/v3"
 	"os/exec"
 	"time"
+
+	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 // Start
 //	@dir: ""if the same dir
 func Start(dir string, etcdPort uint) (*clientv3.Client, error) {
-	// 启动etcd，cmd.Dir填etcd执行文件所在目录
-	cmd := exec.Command("./etcd")
-	if dir != "" {
-		cmd.Dir = dir
+
+	// 调用 ./etcdctl member list 查看状态
+	cmd := exec.Command("./etcdctl", "member", "list")
+	// cmd.Dir 填etcd执行文件所在目录
+	cmd.Dir = "/home/parallels/Downloads/etcd-v3.5.3-linux-arm64"
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout //标准输出内容到out中
+	cmd.Stderr = &stderr //标准输出内容到err中
+
+	err := cmd.Run()
+	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+	fmt.Printf("./etcdctl member list\n")
+	fmt.Printf("out:\n%serr:\n%s", outStr, errStr)
+
+	if err != nil {
+		fmt.Printf("check member list failed, err:%v\n", err)
+		fmt.Printf("[Info] etcd has not started\n")
+	} else {
+		fmt.Printf("[Info] etcd has started\n")
+		goto started
 	}
+
+	// etcd 还未启动，尝试启动
+	cmd = exec.Command("./etcd")
+	cmd.Dir = "/home/parallels/Downloads/etcd-v3.5.3-linux-arm64"
 
 	if err := cmd.Start(); err != nil {
 		fmt.Printf("open etcd failed, err:%v\n", err)
@@ -23,6 +45,7 @@ func Start(dir string, etcdPort uint) (*clientv3.Client, error) {
 	}
 	fmt.Printf("open etcd success\n")
 
+started:
 	endpoint := fmt.Sprintf("127.0.0.1:%d", etcdPort)
 	// 初始化client
 	clientConfig := clientv3.Config{
