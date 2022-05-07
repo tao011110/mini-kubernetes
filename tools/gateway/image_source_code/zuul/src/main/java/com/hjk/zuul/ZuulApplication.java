448e0505c7546c4ad752b4fa0f5df201ab024973
jingkai he <he-jingkai@outlook.com>
2022-05-08 01:32:34 +0800

    add gateway

--- tools/gateway/image_source_code/zuul/src/main/java/com/hjk/zuul/ZuulApplication.java
+++ tools/gateway/image_source_code/zuul/src/main/java/com/hjk/zuul/ZuulApplication.java
@@ -2,7 +2,6 @@ package com.hjk.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
