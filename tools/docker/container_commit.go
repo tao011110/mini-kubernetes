c3434be7323255eb0e344316261b92d83cdd8ab2
陶昱丞 <1501927615@qq.com>
2022-05-18 21:07:11 +0800

    实现了docker commit和docker push，具体打包容器上传镜像的用法见/test/docker_te st.go中示例。在apiserver中提供了deployment和autoscaler的接口(待测试)

--- tools/docker/container_commit.go
+++ tools/docker/container_commit.go
@@ -8,7 +8,8 @@ import (
 )
 
 // CommitContainer 需要传入参数为容器的ID
-func CommitContainer(containerID string) string {
+// 返回值为生成的镜像的ID
+func CommitContainer(containerID string, funcName string) string {
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		fmt.Printf("%v\n", err)
@@ -35,5 +36,11 @@ func CommitContainer(containerID string) string {
 	fmt.Printf("resp.ID is %s\n", resp.ID)
 	fmt.Printf("Image ID is %s\n", resp.ID[7:])
 
-	return resp.ID[7:]
+	imageID := resp.ID[7:]
+
+	// 为镜像打上标签，每个不同的函数生成的镜像区分方式为:
+	// 将函数名funcName作为镜像的 tag
+	TagImage(imageID, funcName)
+
+	return imageID
 }
