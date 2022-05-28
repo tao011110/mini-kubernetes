56a0d9c6199c7291846b52b35d983b1d9352a6d0
陶昱丞 <1501927615@qq.com>
2022-05-28 23:22:46 +0800

    fix a bug in GPUJob create and get; fix a bug in getLocalIP

--- test/master_test/master_test.go
+++ test/master_test/master_test.go
@@ -1,13 +1,12 @@
 package master_test
 
 import (
-	//"mini-kubernetes/tools/master"
-	"net"
+	"mini-kubernetes/tools/master"
 	"testing"
 )
 
 func Test(t *testing.T) {
-	//master.Start()
-	ip := net.ParseIP("127.0.0.1")
-	println(ip.String())
+	master.Start()
+	//ip := net.ParseIP("127.0.0.1")
+	//println(ip.String())
 }
