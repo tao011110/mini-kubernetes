b8bf2f859aaba15286d0f36df0901c8bec43329c
tao-yucheng <taoyucheng@sjtu.edu.cn>
2022-05-29 21:31:23 +0800

    w

--- bin/init_master.sh
+++
@@ -1,9 +0,0 @@
-go build ../tools/master/master.go
-go build ../tools/controller/controller.go
-go build ../tools/scheduler/scheduler.go
-go build ../tools/activer/activer.go
-go build ../tools/kubectl/kubectl.go
-./master &
-./controller &
-./scheduler &
-./activer&
--- bin/init_node.sh
+++
@@ -1,6 +0,0 @@
-go build ../tools/kubelet/kubelet.go
-cp ../tools/kubelet/cadvisor .
-go build ../tools/kubeproxy/proxy.go
-./kubelet &
-./cadvisor &
-./proxy &
