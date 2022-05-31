f07575826412e652f518a44655fd104d7292bf46
陶昱丞 <1501927615@qq.com>
2022-05-31 16:06:24 +0800

    for test

--- presentation/service/service_pre.sh
+++ presentation/service/service_pre.sh
@@ -1,5 +1,5 @@
 ./kubectl create -f /home/go/src/mini-kubernetes/presentation/service/service_pre.yaml
-#查看所有规则iptables sudo iptables -vL -t nat
+#iptables查看所有规则 sudo iptables -vL -t nat
 ./kubectl get services
 ./kubectl describe service test-service-for-deployment
 #⽤⼾能够通过虚拟ip访问Service，由minik8s将请求具体转发⾄对应的pod。 curl 192.168.40.30:80
